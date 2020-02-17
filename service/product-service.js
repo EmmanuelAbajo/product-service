9748f603a15dd6cb5687e59e8e032572a62aa0d3
EmmanuelAbajo <temiabajo@gmail.com>
2020-02-17 11:33:29 +0100

    Refactoring of product service

--- service/product-service.js
+++ service/product-service.js
@@ -1,29 +1,30 @@
 const { ProductRepo } = require('../repository/product-repository');
 const { ObjectID } = require('mongodb');
 
-export class ProductService {
-    getAllProducts(req,res){
-        try {
-            let {count,products} = ProductRepo.getAllProducts();
-            res.status(200).json({count,products});
-         } catch (error) {
-             res.status(400).json({error});
-         }
-    }
+const ProductService = {
+    getAllProducts: (req, res) => {
+        ProductRepo.getAllProducts()
+            .then((data) => {
+                res.status(200).json(data);
+            }).catch((error) => {
+                res.status(400).json({ error });
+            })
+    },
 
-    getProductById(req,res){
+    getProductById: (req, res) => {
         let id = req.params.id;
-            if (!ObjectID.isValid(id)) return res.status(404).json({
+        if (!ObjectID.isValid(id))
+            return res.status(404).json({
                 error: "Invalid id"
             });
-            try {
-                let data = ProductRepo.getProductById(id);
+        ProductRepo.getProductById(id)
+            .then((data) => {
                 res.status(200).json(data);
-            } catch (error) {
+            }).catch((error) => {
                 res.status(400).json(error);
-            }
+            })
     }
 }
 
 
-module.exports = { ProductService }
+module.exports =  ProductService; 
